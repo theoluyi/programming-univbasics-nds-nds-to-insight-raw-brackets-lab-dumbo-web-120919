e4c38b1076ba374aa2622f0a69b068f1ba96798e
Theo Carney <theoluyi@gmail.com>
2019-12-05 22:16:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,12 +2,36 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
+  #  learn --fail-fast
+  # puts directors_database[0][:movies][0][:worldwide_gross] 
+  # this points to first director's first movie's gross 
+  
+  director_gross = {}
+  director_index = 0 
+  while director_index < directors_database.count do
+    movie_gross = 0
+    movie_index = 0 
+    while movie_index < directors_database[director_index][:movies].count do
+      
+    # check that all are movie_gross not plural
+      movie_gross += directors_database[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index +=1 
+    end
+    director_gross[director_index] = movie_gross
+    director_index +=1 
+
+  end
+  director_gross
+end
+
+
+
+
+
+
+
+ # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  # result = {
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -18,5 +42,22 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+
+
+
+
+
+# # FIRST MISWRITE, FORGETTING INSTRUCTIONS/FORMATTING OF OUTPUT
+# def directors_totals(nds)
+#   director_names = []
+#   director_index = 0 
+#   while director_index < directors_database.count do
+#     movie_gross = 0
+#     movie_index = 0 
+#     while movie_index < directors_database[director_index][:movies].count do
+  
+#       movie_gross += directors_database[0][:movies][0][:worldwide_gross] 
+#       movie_index +=1 
+#     end
+    
+#   end 
