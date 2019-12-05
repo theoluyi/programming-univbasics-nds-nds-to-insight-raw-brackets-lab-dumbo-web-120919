20c12a27e3a63a8ac035e9a11eb11e0c52f1ce00
Theo Carney <theoluyi@gmail.com>
2019-12-05 23:22:53 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,36 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  # puts directors_database[0][:movies][0][:worldwide_gross] 
+  # this points to first director's first movie's gross 
+  
+  director_gross = {}
+  director_index = 0 
+  while director_index < nds.count do
+    movie_gross = 0
+    movie_index = 0
+    
+    while movie_index < nds[director_index][:movies].count do
+      
+      movie_gross += nds[director_index][:movies][movie_index][:worldwide_gross] #√
+      # puts nds[director_index][:movies][movie_index][:worldwide_gross] #√
+      # by above puts test, this is successfully iterating thru A) movies and 
+      # B) directors 
+      movie_index +=1 #√
+      
+    end
+    
+    #  learn --fail-fast
+    director_name = nds[director_index][:name]
+    puts director_name
+    
+    director_gross[director_name] = movie_gross
+    
+    # director_gross[director_index] = movie_gross
+    # above is totally wrong hash key value assignment step 
+    # I have tried to correct it but the correction may have issues
+    director_index +=1 
+
+  end
+  director_gross #return vs puts?
 end
