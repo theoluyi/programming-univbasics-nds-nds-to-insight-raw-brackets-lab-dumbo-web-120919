54ca91b9525d62df77f2a88b4c0079debd1af57e
Theo Carney <theoluyi@gmail.com>
2019-12-05 22:44:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,16 +6,16 @@ def directors_totals(nds)
   # puts directors_database[0][:movies][0][:worldwide_gross] 
   # this points to first director's first movie's gross 
   
-  puts nds[0][:movies][0][:worldwide_gross] 
+  
   director_gross = {}
   director_index = 0 
-  while director_index < directors_database.count do
+  while director_index < nds.count do
     movie_gross = 0
     movie_index = 0 
     
-    while movie_index < directors_database[director_index][:movies].count do
+    while movie_index < nds[director_index][:movies].count do
       
-      movie_gross += directors_database[director_index][:movies][movie_index][:worldwide_gross] #√
+      movie_gross += nds[director_index][:movies][movie_index][:worldwide_gross] #√
       movie_index +=1 #√
     end
     
